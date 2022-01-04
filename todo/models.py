932bc9e85b562704c8ad57e4381868659d50830d
DerLed <lediburit@mailru>
2022-01-04 12:04:04 +0300

    make app todo

---
+++ todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок задачи')
+    description = models.TextField(max_length=400, verbose_name='Описание задачи')
+    is_solved = models.BooleanField(default=False, verbose_name='Выполнено')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
