6812fb013ce9398416deb95c53fd2ae2535e8f11
DerLed <lediburit@mailru>
2022-01-16 22:41:53 +0300

    any

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,10 @@ class Todo(models.Model):
     description = models.TextField(max_length=400, verbose_name='Описание задачи')
     is_solved = models.BooleanField(default=False, verbose_name='Выполнено')
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date_published = models.DateTimeField('date_published', auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ( 'is_solved', '-date_published',)
